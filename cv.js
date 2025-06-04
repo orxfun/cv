8c89533578e14729c011b816f95e7b306c024210
orxfun <orx.ugur.arikan@gmail.com>
2025-06-04 10:55:26 +0200

    media queries added for mobile view

--- cv.js
+++ cv.js
@@ -7,29 +7,35 @@ document.addEventListener("DOMContentLoaded", () => {
 function setDarkModeEvents() {
     function getDarkModeDiv(className) {
         const buttons = document.getElementsByClassName(className);
-        return buttons.length === 0 ? null : buttons[0]
+        return buttons.length !== 2 ? null : buttons
     }
     const moon = getDarkModeDiv("moon");
     const sun = getDarkModeDiv("sun");
 
     function setModeMoon() {
-        moon.style.display = "inherit";
-        sun.style.display = "none";
+        moon[0].style.display = "inherit";
+        sun[0].style.display = "none";
+        moon[1].style.display = "inherit";
+        sun[1].style.display = "none";
         document.documentElement.style.setProperty('--bg-color', "black");
         document.documentElement.style.setProperty('--color', "white");
         setThemeColor('#ff3300');
     }
 
     function setModeSun() {
-        moon.style.display = "none";
-        sun.style.display = "inherit";
+        moon[0].style.display = "none";
+        sun[0].style.display = "inherit";
+        moon[1].style.display = "none";
+        sun[1].style.display = "inherit";
         document.documentElement.style.setProperty('--bg-color', "white");
         document.documentElement.style.setProperty('--color', "black");
         setThemeColor('#006bb3');
     }
 
-    moon?.addEventListener('click', function (_) { setModeSun() });
-    sun?.addEventListener('click', function (_) { setModeMoon() });
+    moon[0]?.addEventListener('click', function (_) { setModeSun() });
+    sun[0]?.addEventListener('click', function (_) { setModeMoon() });
+    moon[1]?.addEventListener('click', function (_) { setModeSun() });
+    sun[1]?.addEventListener('click', function (_) { setModeMoon() });
 }
 
 function setThemeColor(color) {
